d6ffc51f1510b1ac1d5fa135c25667ccf5893ae2
Realgrapejuice <66715905+realgrapejuice@users.noreply.github.com>
2020-08-03 22:07:27 +0900

    todo-list / JS

---
+++ Todo-list/app.js
@@ -0,0 +1,71 @@
+const todoForm = document.querySelector(".todo_input"),
+  todoInput = todoForm.querySelector("input"),
+  submitBtn = todoForm.querySelector("button"),
+  todoUl = document.querySelector("ul");
+
+const USER_LS = "userTodo";
+let todoArray = [];
+
+const saveTodo = () => {
+  localStorage.setItem(USER_LS, JSON.stringify(todoArray));
+};
+
+const delTodo = () => {
+  const delBtn = event.target;
+  const li = delBtn.parentNode;
+  todoUl.removeChild(li);
+  const cleanList = todoArray.filter(function (todo) {
+    return todo.id !== parseInt(li.id);
+  });
+  todoArray = cleanList;
+  saveTodo();
+};
+
+const askTodo = (todo) => {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const todoId = todoArray.length + 1;
+  todoUl.appendChild(li);
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = todoId;
+  delBtn.innerText = "remove";
+  delBtn.addEventListener("click", delTodo);
+  span.innerText = todo;
+  todoObj = {
+    todo: todo,
+    id: todoId,
+  };
+  todoArray.push(todoObj);
+  saveTodo();
+};
+
+const submitEventhandler = (event) => {
+  event.preventDefault();
+  const currentTodo = todoInput.value;
+  if (currentTodo.length == 0) {
+    todoInput.placeholder = `You didn't input anything!`;
+  } else {
+    todoInput.placeholder = `New-todo-item`;
+    askTodo(currentTodo);
+    todoInput.value = "";
+  }
+};
+
+const loadTodo = () => {
+  const currentTodo = localStorage.getItem(USER_LS);
+  if (currentTodo !== null) {
+    const parsedTodo = JSON.parse(currentTodo);
+    parsedTodo.forEach(function (todo) {
+      askTodo(todo.todo);
+    });
+  }
+};
+
+const initTodo = () => {
+  loadTodo();
+  todoForm.addEventListener("submit", submitEventhandler);
+};
+
+initTodo();
