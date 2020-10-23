1a24bb16740867f7e30d26b81985c08dfccc1b56
Realgrapejuice <66715905+realgrapejuice@users.noreply.github.com>
2020-10-23 18:10:17 +0900

    JS: todo save part

--- weather/src/index.js
+++ weather/src/index.js
@@ -4,11 +4,13 @@ import Background from "./background.js";
 import Clock from "./clock.js";
 import Greeting from "./greeting.js";
 import Focus from "./focus.js";
+import Todo from "./todo.js";
 
 const background = new Background();
 const currentTime = new Clock();
 const greeting = new Greeting();
 const focus = new Focus();
+const todo = new Todo();
 
 function init() {
   background.getBackgroundImg();
--- weather/src/todo.js
+++ weather/src/todo.js
@@ -1,5 +1,90 @@
 "use strict";
 
-class Todo {
-  constructor;
+export default class Todo {
+  constructor() {
+    this.todoContainer = document.querySelector(".todo__container");
+    this.ol = document.querySelector(".todo__list--container");
+    this.todoSwitchBtn = document.querySelector(".todo__switch");
+    this.todoSwitchBtn.addEventListener("click", this._visualizeTodo);
+    this.todoForm = document.querySelector(".todo__form");
+    this.todoForm.addEventListener("submit", () => {
+      this._submitEventHandler();
+    });
+
+    this.status = false;
+    this.CLASS_VISIBLE = "todo__visible";
+    this.TODO_LS = "userTodo";
+    this.USER_OBJ = {};
+    this.count = 0;
+  }
+
+  _loadTodo = () => {
+    const todoObj = localStorage.getItem(this.TODO_LS);
+    const fromJSONObj = JSON.parse(todoObj);
+    return fromJSONObj;
+  };
+
+  _deleteTodo = (todoObj) => {};
+
+  _saveTodo = (todoObj) => {
+    const toJSONObj = JSON.stringify(todoObj);
+    localStorage.setItem(this.TODO_LS, toJSONObj);
+  };
+
+  _makeHTML = (todoObj) => {
+    const li = document.createElement("li");
+    const input = document.createElement("input");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    li.setAttribute("class", "todo__item");
+    li.id = todoObj.id;
+    input.setAttribute("class", "todo__checkbox");
+    input.setAttribute("type", "checkbox");
+    span.setAttribute("class", "todo__desc");
+    span.textContent = todoObj.todo;
+    button.setAttribute("class", "todo__btn");
+    button.setAttribute("type", "button");
+    button.textContent = `X`;
+    button.addEventListener("click", (event) => {
+      const li = event.target.parentNode;
+      this.count--;
+      if (todoObj.id === parseInt(li.id)) {
+        this.ol.removeChild(li);
+      }
+    });
+    li.append(input, span, button);
+    this.ol.append(li);
+  };
+
+  _submitEventHandler = () => {
+    this.count++;
+    if (this.count > 8) {
+      this.count--;
+      alert(`What if we deal with priority things first?`);
+      return;
+    }
+    const todoInput = this.todoForm.querySelector("input");
+    const userTodo = todoInput.value;
+    todoInput.value = "";
+    const todoObj = {
+      id: Date.now(),
+      todo: userTodo,
+    };
+    this.USER_OBJ.id = this.count;
+    this.USER_OBJ.item = todoObj;
+    this._makeHTML(todoObj);
+    this._saveTodo(todoObj);
+  };
+
+  _visualizeTodo = () => {
+    if (!this.status) {
+      this.status = true;
+      this.todoContainer.classList.add(this.CLASS_VISIBLE);
+    } else {
+      this.status = false;
+      this.todoContainer.classList.remove(this.CLASS_VISIBLE);
+    }
+  };
 }
+
+// 진행상황: saveTodo가 제대로 작동하도록 만들고 있는 상황, 이때 저장한 객체가 차곡차곡 쌓이지 않고, 단발적으로 로컬 스토리지에 저장되는 문제가 있는 상황임. 즉, todo 객체를 만들지 못하고 있는 상황인데, 이걸 어떻게 해결하면 좋을지에 대해 미지수임
